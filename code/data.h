bb421573838e21736efd2fab07bd98b48279bd6f
XezbethMultimedia <99296364@students.camre.ac.uk>
2024-10-23 10:03:49 +0100

    started movement scripts.

--- code/data.h
+++ code/data.h
@@ -1,5 +1,25 @@
 #pragma once
 
 #include <iostream>
+#include <map>
 
 struct Vector2 { int x, y; };
+
+enum Commands {move, attack, commands, stop};
+enum Movement {N, E, S, W};
+
+std::map<std::string, Commands> stringToCommand =
+{
+	{"move", move},
+	{"attack", attack},
+	{"commands", commands},
+	{"stop", stop}
+};
+
+std::map<char, Movement> charToMovement =
+{
+	{'n', N},
+	{'e', E},
+	{'s', S},
+	{'w', W}
+};
--- code/things.h
+++ code/things.h
@@ -18,3 +18,24 @@ public:
 	int psi = 1;
 	int hp = 5;
 };
+
+class Player : public Entity {
+public:
+	Entity move(Entity entity) {
+		Movement movement;
+		char input;
+
+		std::cout << "Which direction?(n/e/s/w): ";
+		std::cin >> input;
+		movement = charToMovement[input];
+
+		switch (movement) {
+		case 0:
+			entity.location.x += 1;
+			break;
+		case 1:
+			entity.location.y += 1;
+			break;
+		}
+	}
+};
